e4f8a1e70226a209828cb51c1078b0f54408b995
Riley Adams <rileya@cs.washington.edu>
2013-04-28 21:15:09 -0700

    Assorted fixes.

--- media/materials/emit_fp.glsl
+++ media/materials/emit_fp.glsl
@@ -5,7 +5,7 @@ uniform sampler2D diffuseMap;
 varying vec2 uv;
 
 void main() {
-  float eye_dist = length(pos - eye_pos);
+  float eye_dist = length(pos.xyz - eye_pos.xyz);
   float fog = clamp((eye_dist - 150.0) / 150.0,0.0,1.0);
   gl_FragColor = mix(texture2D(diffuseMap, uv), vec4(1.0,1.0,1.0,1.0), fog); 
 }
--- media/materials/ruins_fp.glsl
+++ media/materials/ruins_fp.glsl
@@ -5,11 +5,9 @@ uniform sampler2D diffuseMap;
 varying vec2 uv;
 
 void main() {
-  //vec3 sun = normalize(vec3(0.5,0.5,0.3));
-  //float c = max(dot(normal, sun.xyz), 0);
 
-  float eye_dist = length(pos - eye_pos);
+  float eye_dist = length(pos.xyz - eye_pos.xyz);
   float fog = clamp((eye_dist - 150.0) / 150.0,0.0,1.0);
 
-  gl_FragColor = mix(texture2D(diffuseMap, uv) * 0.8, vec4(1.0,1.0,1.0,1.0), fog); 
+  gl_FragColor = mix(texture2D(diffuseMap, uv).xyzw * 0.8, vec4(1.0,1.0,1.0,1.0), fog); 
 }
